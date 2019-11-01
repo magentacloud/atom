a96bf7246fe2f443892231410a0b20bf2ec2d177
magentacloud <47084755+magentacloud@users.noreply.github.com>
2019-11-01 09:10:34 +0300

    checkstyle for tests

--- homeworks/HW2/src/test/java/GameTest.java
+++ homeworks/HW2/src/test/java/GameTest.java
@@ -1,14 +1,18 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 public class GameTest {
     Game game = new Game();
+
     @Test
     public void getWordFromDictionaryTest() {
         String str = game.getWordFromDictionary();
         assertNotNull(str);
     }
+
     @Test
     public void countCowsTest() {
         assertEquals(game.countCows("java", "lava"), 3);
